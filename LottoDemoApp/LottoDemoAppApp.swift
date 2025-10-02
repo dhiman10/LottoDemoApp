5385182f068cfb2514220a6e5fb8aeff9ff49258
dhiman10 <dhimandas10.sec@gmail.com>
2025-10-02 13:52:22 +0200

    Initial Commit

---
+++ LottoDemoApp/LottoDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottoDemoAppApp.swift
+//  LottoDemoApp
+//
+//  Created by Dhiman Das on 02.10.25.
+//
+
+import SwiftUI
+
+@main
+struct LottoDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
